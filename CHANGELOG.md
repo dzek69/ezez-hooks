bab4171e9af3cff276baee0f4fdc5a6dff9f2522
dzek <git-public@dzek.eu>
2023-06-29 20:20:51 +0200

    ! missing `useSimpleGeolocation` export

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 (nothing yet)
 
 ## [1.1.0] - 2023-06-29
+### Fixed
+- missing `useSimpleGeolocation` export
+
+## [1.1.0] - 2023-06-29
 ### Added
 - `useGeolocation` hook
 - `useSimpleGeolocation` hook
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from "./useConditionalHooks.js";
 export * from "./useForceUpdate.js";
 export * from "./useGeolocation.js";
 export * from "./useInputDevice.js";
+export * from "./useSimpleGeolocation.js";
 export * from "./useToday.js";
 export * from "./useUpdateEvery.js";
